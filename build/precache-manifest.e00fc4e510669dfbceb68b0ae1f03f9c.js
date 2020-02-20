07bedfee869f15ec8b425426052368429327fa27
juancvidal <jucarvidal@gmail.com>
2020-02-20 12:13:06 -0500

    Remove the now ignored directory node_modules

---
+++ build/precache-manifest.e00fc4e510669dfbceb68b0ae1f03f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71744dac63ae821e8d74c9621dc2cb32",
+    "url": "/torre/index.html"
+  },
+  {
+    "revision": "09ae933dcb6137f972c2",
+    "url": "/torre/static/css/main.1e46f545.chunk.css"
+  },
+  {
+    "revision": "67892ad285260fdb43c3",
+    "url": "/torre/static/js/2.0633848c.chunk.js"
+  },
+  {
+    "revision": "09ae933dcb6137f972c2",
+    "url": "/torre/static/js/main.b132fdfc.chunk.js"
+  },
+  {
+    "revision": "89cda03b9d3e3beb12a8",
+    "url": "/torre/static/js/runtime-main.2928c7af.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/torre/precache-manifest.652d3cf84a74799af672e11f5011a5c1.js"
+  "/torre/precache-manifest.e00fc4e510669dfbceb68b0ae1f03f9c.js"
 );
 
 self.addEventListener('message', (event) => {
